f5dd9772572458741c833ef34b643b2372a1dc75
Arcady "RKD" Chumachenko <arcady.chumachenko@gmail.com>
2026-01-15 16:59:37 +0000

    Add coverage output to integration tests

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -28,13 +28,15 @@ run_mode() {
   fi
 
   (cd "${ROOT_DIR}" && \
+    mkdir -p coverage && \
     TEST_MODE=${mode} \
     PROXY_URL=http://localhost:8080 \
     ES_URL=http://localhost:9200 \
     ALIAS_TEMPLATE=$(grep '^ES_TMNT_SHARED_INDEX_ALIAS_TEMPLATE=' "${ROOT_DIR}/${env_file}" | cut -d'=' -f2-) \
     TENANT_FIELD=$(grep '^ES_TMNT_SHARED_INDEX_TENANT_FIELD=' "${ROOT_DIR}/${env_file}" | cut -d'=' -f2-) \
     REAL_INDEX=$(grep '^ES_TMNT_INDEX_PER_TENANT_TEMPLATE=' "${ROOT_DIR}/${env_file}" | cut -d'=' -f2-) \
-    go test ./tests -run "${test_name}")
+    go test ./tests -run "${test_name}" -coverprofile "coverage/integration-${mode}.out" -coverpkg=./... && \
+    go tool cover -func "coverage/integration-${mode}.out")
 
   docker compose -f "${ROOT_DIR}/docker-compose.yml" --env-file "${ROOT_DIR}/${env_file}" down -v
 }
